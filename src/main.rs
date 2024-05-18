437121d0432a02ce801b0d3c15c9b771d1fb9aec
Lawand Othman <lwnd@pm.me>
2024-05-18 12:50:13 +0100

    initial schema and resolvers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,87 @@
 use std::error::Error;
 
-use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, Schema};
+use async_graphql::{
+    http::GraphiQLSource, Context, EmptyMutation, EmptySubscription, Enum, FieldResult, Object,
+    Schema, SimpleObject,
+};
 use async_graphql_poem::*;
+use chrono::{DateTime, Utc};
 use poem::{listener::TcpListener, web::Html, *};
+
+#[derive(Enum, Copy, Clone, Eq, PartialEq)]
+enum SportId {
+    #[graphql(name = "ARCHERY")]
+    Archery,
+    #[graphql(name = "SPORT_CLIMBING")]
+    SportClimbing,
+}
+
+#[derive(SimpleObject)]
+struct Location {
+    id: i32,
+    name: String,
+    events: Vec<Event>,
+}
+
+#[derive(SimpleObject)]
+struct Sport {
+    id: SportId,
+    name: String,
+    events: Vec<Event>,
+}
+
+#[derive(SimpleObject)]
+struct Event {
+    sport: Sport,
+    name: String,
+    time: DateTime<Utc>,
+    location: Location,
+}
+
+#[derive(SimpleObject)]
+struct Day {
+    events: Vec<Event>,
+}
 
 struct Query;
 
 #[Object]
 impl Query {
-    async fn howdy(&self) -> &'static str {
-        "partner"
+    async fn events(&self, _ctx: &Context<'_>) -> FieldResult<Vec<Event>> {
+        Ok(vec![])
+    }
+
+    async fn event(&self, _ctx: &Context<'_>) -> FieldResult<Event> {
+        Ok(Event {
+            sport: Sport {
+                id: SportId::Archery,
+                name: "Archery".to_string(),
+                events: vec![],
+            },
+            name: "Event Name".to_string(),
+            time: Utc::now(),
+            location: Location {
+                id: 1,
+                name: "Location Name".to_string(),
+                events: vec![],
+            },
+        })
+    }
+
+    async fn sports(&self, _ctx: &Context<'_>) -> FieldResult<Vec<Sport>> {
+        Ok(vec![])
+    }
+
+    async fn sport(&self, _ctx: &Context<'_>, id: SportId) -> FieldResult<Sport> {
+        Ok(Sport {
+            id,
+            name: "Sport Name".to_string(),
+            events: vec![],
+        })
+    }
+
+    async fn schedule(&self, _ctx: &Context<'_>) -> FieldResult<Day> {
+        Ok(Day { events: vec![] })
     }
 }
 
